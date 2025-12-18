8eff1fd1cba45587bc73fc5e3207a4c10cdb04e2
VishalR133 <vishal.r2023c@vitstudent.ac.in>
2025-12-18 16:50:55 +0530

    Login related code

--- add.py
+++ add.py
@@ -5,3 +5,11 @@ int b=input("Enter the value of b: ")
 int add=a+b
 print(add)
 print ("Modified the content")
+valid_username="admin"
+valid_password="1234"
+username=input("Enter the username: ")
+password=input("Enter the Password: ")
+if username==valid_username and password==valid_password:
+    print("Login Successful!")
+else:
+    print("Login failed")
