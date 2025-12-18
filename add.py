ebc3d83956c4ca4282bacb8e27c552fcb30d9e2c
VishalR133 <vishal.r2023c@vitstudent.ac.in>
2025-12-18 17:01:01 +0530

    Merge commit

--- add.py
+++ add.py
@@ -1,9 +1,5 @@
 print("Hello World!!")
 print("Hii")
-int a=input("Enter the value of a: ")
-int b=input("Enter the value of b: ")
-int add=a+b
-print(add)
 print ("Modified the content")
 valid_username="admin"
 valid_password="1234"
